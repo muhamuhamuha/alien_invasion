6794ed18b6fbdc81255fd14d1b5f04d40d858841
muhamuhamuha <51298421+muhamuhamuha@users.noreply.github.com>
2019-11-18 23:15:41 -0500

    added multiple alien rows

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
     def __init__(self):
         """Initializing the game's settings."""
         # Screen settings
-        self.screen_width = 1000
+        self.screen_width = 1050
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
 
