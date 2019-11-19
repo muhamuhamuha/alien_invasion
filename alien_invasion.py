5767340752704e9610e61cbe890dfcc274668707
muhamuhamuha <51298421+muhamuhamuha@users.noreply.github.com>
2019-11-18 20:33:29 -0500

    adding basic bullet groups

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -15,14 +16,19 @@ def run_game():
     # Make a ship.
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets in
+    bullets = Group()
+
     # Set the background color
     bg_color = (230, 230, 230)
 
     # Start the main loop for the game
     while True:
 
+        gf.check_events(ai_settings, screen, ship, bullets)
         gf.check_events(ship)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
        
 run_game()
