2d7d517d638994b5be27dbcafa1285601462c8b7
muhamuhamuha <51298421+muhamuhamuha@users.noreply.github.com>
2019-11-24 14:56:42 -0500

    creating settings for fleet direction

--- settings.py
+++ settings.py
@@ -20,5 +20,8 @@ class Settings:
         
         # Alien settings
         self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        # fleet_direction 1 represents right; -1 repr left
+        self.fleet_direction = 1
         
     
