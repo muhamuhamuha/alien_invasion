6545c91f2c31057fb14183a0f3bfb8d04de02214
muhamuhamuha <51298421+muhamuhamuha@users.noreply.github.com>
2019-11-24 14:54:50 -0500

    moving the aliens right

--- settings.py
+++ settings.py
@@ -17,3 +17,8 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (0, 0, 255)
         self.bullets_allowed = 3
+        
+        # Alien settings
+        self.alien_speed_factor = 1
+        
+    
